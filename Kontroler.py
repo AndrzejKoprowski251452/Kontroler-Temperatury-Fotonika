3c868703683a70d7a64c4a87180be4c5053ca225
AndrzejKoprowski <251452@edu.p.lodz.pl>
2024-11-06 17:14:36 +0100

    v23

--- Kontroler.py
+++ Kontroler.py
@@ -50,7 +50,7 @@ class App(Tk):
 
     def update_graph(self):
         self.frame.update_graph()
-        self.after(10, self.update_graph)
+        self.after(50, self.update_graph)
         
     def on_closing(self):
         answer = messagebox.askquestion("Warning", "Do you want to save the data?", icon="warning")
@@ -71,7 +71,7 @@ class App(Tk):
             self.frame.fig.savefig(save_path)
             json_path = os.path.join(dir, "data.json")
             with open(json_path, "w") as json_file:
-                json.dump({"temp":self.frame.data,"cur":self.frame.current,"time":self.frame.time}, json_file,indent=4)
+                json.dump({"temp":self.frame.data,"time":self.frame.time}, json_file,indent=4)
         window.destroy()
         
     def import_config(self):
@@ -101,10 +101,9 @@ class StartPage(LabelFrame):
         self.controller = controller
         
         self.data = [0]
-        self.current = [0]
         self.time = [0]
         self.sent_data_value = 20.0
-        self.buffor = ['*GETTACT;','*GETIOUT;','A']
+        self.buffor = ['*GETTPRS;','*GETTACT;','A']
         self.time_start = time.time()
         self.time_change = time.time()
         self.r = 1
@@ -121,14 +120,6 @@ class StartPage(LabelFrame):
 
         self.set_data_label = Label(self,text=f"Set Temp. : {self.sent_data_value}°C")
         self.set_data_label.grid(row=0, column=2,sticky='w')
-        
-        self.current_off_v = IntVar()
-        self.current_off_v.set(config['current_off'])
-        self.current_off = Checkbutton(self,variable=self.current_off_v,onvalue=True,offvalue=False,command=self.change_current)
-        self.current_off.grid(row=0,column=2,sticky='e')
-        
-        self.current_off_text = Label(self,text=f"Current flowing: {bool(self.current_off_v.get())}")
-        self.current_off_text.grid(row=0,column=3,sticky='w')
 
         self.measure_time = Label(self)
         self.measure_time.grid(row=0, column=4)
@@ -145,14 +136,11 @@ class StartPage(LabelFrame):
         self.fig = Figure(figsize=(5, 5), dpi=100)
         self.fig.patch.set_facecolor('#F0F0F0')
         self.ax1 = self.fig.add_subplot(1,1,1)
-        self.ax2 = self.ax1.twinx()
         self.ax1.grid()
         self.line, = self.ax1.plot(self.time,self.data, 'g')
-        self.current_data_line, = self.ax2.plot(self.time,self.current, 'orange')
         maxv = float(controller.temp.get().split(' /')[1])
         minv = float(controller.temp.get().split(' /')[0])
         self.last_data_text = self.ax1.text(0, 0, "0", ha='right', va='bottom', fontsize=8, color='red')
-        self.last_current_text = self.ax2.text(0, 0, "0", ha='right', va='bottom', fontsize=8, color='orange')
         self.up_range_text = self.ax1.text(self.time[-1]*0.8, maxv, f"min: {maxv:.3f}°C", ha='right', va='top', fontsize=8, color='grey')
         self.down_range_text = self.ax1.text(self.time[-1]*0.8, minv, f"max: {minv:.3f}°C", ha='right', va='bottom', fontsize=8, color='grey')
         self.up_range = self.ax1.axhline(y=maxv, color='grey', linestyle='--')
@@ -163,14 +151,9 @@ class StartPage(LabelFrame):
         self.canvas = FigureCanvasTkAgg(self.fig, master=self)
         self.canvas.draw()
         self.canvas.get_tk_widget().grid(row=0, column=0, sticky="nsew",rowspan=20)
-    def change_current(self):
-        config['current_off'] = bool(self.current_off_v.get())
-        with open('config.json', "w") as config_file:
-            json.dump(config, config_file)
-        self.current_off_text.config(text=f"Current flowing: {bool(self.current_off_v.get())}")
-        
+                
     def console_data(self,f):
-        self.console.insert(INSERT, f'{(time.time() - self.time_start):.2f}: {f}\n')
+        self.console.insert(INSERT, f'{(time.time() - self.time_change):.2f}: {f}\n')
         self.console.see("end")
 
     def update_graph(self):
@@ -179,27 +162,24 @@ class StartPage(LabelFrame):
             if config['current_off']:
                 self.buffor = ['a']
             elif 'a' in self.buffor and not config['current_off']:
-                self.buffor = ['*GETTACT;','*GETIOUT;','A']
+                self.buffor = ['*GETTPRS;','*GETTACT;','A']
             for b in self.buffor:
                 if not self.controller.connection.in_waiting:
                     self.controller.connection.write(str.encode(b))
                 v = self.controller.connection.readline().decode('Latin-1')
+                if '*TPRS ' in v:
+                    self.sent_data_value = float(v[5:12])
                 if '*TACT ' in v:
                     self.data.append(float(v[5:12]))
-                    #print(f'Temperature: {v[5:12]}C')
+                    print(f'Temperature: {v[5:12]}C')
                     if self.data[-1] >= float(self.controller.temp.get().split(' /')[1]):
                         config['current_off'] = True
                     else:
                         config['current_off'] = False
-                if '*IOUT ' in v:
-                    c = float(v[9:15].replace('A',''))
-                    self.current.append(c)
-                    #print(f'Current: {c}A')
-                #if '*CK ' in v and "*GETCK;" in self.buffor:
-                    #self.controller.v = v[3:].split(' ')
                 self.controller.connection.flush()
                 self.buffor = self.buffor[:2]
-                
+        self.sent_data_line.set_ydata([self.sent_data_value])
+        self.set_data_label.config(text=f"Set Temp. : {self.sent_data_value}°C")
         if len(self.data) > len(self.time):
             self.time.append(float(timev))
         if self.time[-1] > 20 and self.controller.graphFold:
@@ -207,14 +187,9 @@ class StartPage(LabelFrame):
         elif not self.controller.graphFold:
             self.r = 0.1
         self.line.set_data(self.time, self.data)
-        if len(self.time) == len(self.current) == len(self.data):
-            self.current_data_line.set_data(self.time,self.current)
+        if len(self.time) == len(self.data):
             self.ax1.set_xlim(self.r, self.time[-1])
             self.ax1.set_ylim(min(self.data) - 20, max(self.data) + 20)
-            self.ax2.set_xlim(self.r, self.time[-1])
-            self.ax2.set_ylim(min(self.current) - 20, max(self.current) + 20)
-        else:
-            print("t",len(self.time),"c",len(self.current),"d",len(self.data),len(self.time) == len(self.current) == len(self.data))
         maxv = float(min(float(self.controller.temp.get().split(' /')[1]), max(self.data) + 20))
         minv = float(max(float(self.controller.temp.get().split(' /')[0]), min(self.data) - 20))
         self.down_range_text.set_position((self.time[-1], maxv))
@@ -223,14 +198,13 @@ class StartPage(LabelFrame):
         self.up_range_text.set_text(f"min: {minv:.1f}°C")
         self.last_data_text.set_position((self.time[-1] - 1, self.data[-1]))
         self.last_data_text.set_text(f"{self.data[-1]}°C")
-        self.last_current_text.set_position((self.time[-1] - 1, self.current[-1]))
-        self.last_current_text.set_text(f"{self.current[-1]}A")
         self.last_data_label.config(text=f"Current Temp. : {self.data[-1]}°C")
         self.measure_time.config(text=f"Time : {timev}s")
         self.changed_time.config(text=f'Time of measure : {(time.time() - self.time_change):.2f}s')
         self.canvas.draw()
 
     def send_serial_data(self):
+        print('-------------------------------------')
         self.time_change = time.time()
         m = self.controller.temp.get().split(' /')
         v = 0
@@ -244,10 +218,8 @@ class StartPage(LabelFrame):
             print(f'Set Temperatur: {self.sent_data_value}°C')
         else:
             print("No divice connected")
-        self.sent_data_line.set_ydata([self.sent_data_value])
         self.up_range.set_ydata([float(m[1])])
         self.down_range.set_ydata([float(m[0])])
-        self.set_data_label.config(text=f"Set Temp. : {self.sent_data_value}°C")
         self.update_graph()
         self.entry.delete(0, len(str(self.sent_data_value)))
 
@@ -304,7 +276,6 @@ class Options(Toplevel):
         config['fold'] = bool(self.v.get())
     def Save(self):
         self.controller.frame.buffor.append(f'*SETCK{self.p.get()} {self.i.get()} {self.d.get()};')
-        #self.controller.connection.write(str.encode(f'*SETCK{self.p.get()} {self.i.get()} {self.d.get()};'))
         self.controller.v = [self.p.get(), self.i.get(), self.d.get()]
         config['pid'] = [self.p.get(), self.i.get(), self.d.get()]
         config['port'] = self.controller.port.get()
